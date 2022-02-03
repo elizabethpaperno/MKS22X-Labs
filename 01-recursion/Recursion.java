672736e553bad34eabd250c7de8525161ff39b6c
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-03 10:43:23 -0500

    fill in printAllWords method and add skeleton for the rest

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -12,5 +12,38 @@ public class Recursion{
     */
     public static void printAllWords(int length,String word){
       //WRITE THIS METHOD
+        if (length > 0){
+          for (char c = 'a'; c <= 'e'; c++){
+            printAllWords(length - 1, word + c);
+          }
+        }
+        else{
+          System.out.print(word);
+        }
+    }
+
+    /*Print all words that are made of the characters in the array of letters.
+    *There may not be consecutive equal letters, so:
+    *aax is not allowed, but axa is allowed.
+    *@param length : the length of the words that are to be printed
+    *@param letters: the letters you should be using,
+    *@precondition: letters contains at least 2 characters, and has no duplicates.
+    */
+    public static void printNoDoubleLetterWords(int length,char[] letters){
+      printNoDoubleLetterWords(length,"",letters);
+    }
+
+    /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+    *aax is not allowed, but axa is allowed.
+    *@param length : either how many more letters need to be
+    *@param word   : the partial word so far.
+    *@param letters: the letters you should be using
+    */
+    public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+      //WRITE THIS METHOD
+    }
+
+    public static void main(String[] args){
+      printAllWords(3, "");
     }
 }
