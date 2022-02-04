209f1604a05d2968a4a2a95a24357a10ef8c31b2
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-04 10:45:26 -0500

    new methods to add

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -51,6 +51,40 @@ public class Recursion{
       }
     }
 
+    /*
+          *@param s any string
+          *@return a string that is the reversed version of s, do NOT use built in methods to do so, use recursion.
+          */
+    public static String reverse(String s){
+
+    }
+
+    /*
+          *@param length how long the words must be
+          *param word the variable to store the partial solution (should start at "")
+          *@return the number of words that have no adjacent matching letters using the letters a-z.
+          *Repetition allowed except when letters are adjacent.
+          */
+    public static long countNoDoubleLetterWords(int length,String word){
+      //Hint: not a wrapper method, but you must call it starting with "" as your word.
+    }
+
+    /*
+          *@param n any non-negative value you want to take the sqrt of
+          *@return the approximate sqrt of n within a tolerance of 0.001%
+          */
+    public static double sqrt(double n){
+      //Hint: This is a wrapper method.
+    }
+
+    /*
+        *@param n any non-negative value
+        *@return the nth term of the fibonacci sequence. 0, 1, 1, 2, 3, 5 etc.
+        */
+    public int fibIter(int n, int f1, int f2){
+      //DO NOT call fibIter more than once
+    }
+    
     public static void main(String[] args){
       System.out.println("og method: ");
       printAllWords(3);
