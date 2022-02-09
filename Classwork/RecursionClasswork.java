c794f0519b055320e15f4d6d381a9dfdc69cdf1b
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-09 10:30:27 -0500

    RecursionClasswork added first three methods

---
+++ Classwork/RecursionClasswork.java
@@ -0,0 +1,39 @@
+public class RecursionClasswork{
+  /*
+    Given an array of ints, is it possible to choose a group of some of the ints,
+    such that the group sums to the given target?
+    This is a classic backtracking recursion problem.
+    Once you understand the recursive backtracking strategy in this problem,
+    you can use the same pattern for many problems to search a space of choices.
+    Rather than looking at the whole array, our convention is to consider the part of the array
+    starting at index start and continuing to the end of the array.
+    The caller can specify the whole array simply by passing start as 0.
+    No loops are needed -- the recursive calls progress down the array.
+  */
+  public static boolean groupSum(int start, int[] nums, int target) {
+
+  }
+
+  /*
+    Given an array of ints, is it possible to divide the ints into two groups,
+    so that the sums of the two groups are the same.
+    Every int must be in one group or the other.
+    Write a recursive helper method that takes whatever arguments you like,
+    and make the initial call to your recursive helper from splitArray().
+    (No loops needed.)
+  */
+
+  public static boolean splitArray(int[] nums) {
+
+  }
+
+  /*
+  Given an array of ints, is it possible to choose a group of some of the ints,
+  beginning at the start index, such that the group sums to the given target?
+  However, with the additional constraint that all 6's must be chosen. (No loops needed.)
+  */
+
+  public static boolean groupSum6(int start, int[] nums, int target) {
+
+  }
+}
