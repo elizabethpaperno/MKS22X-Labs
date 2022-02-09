c93dfb0ddf07cc45e3de5498ebe2dbb1590824f1
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-09 10:59:48 -0500

    RecursionClasswork progress on groupSum6 -> not working

--- Classwork/RecursionClasswork.java
+++ Classwork/RecursionClasswork.java
@@ -47,6 +47,12 @@ public class RecursionClasswork{
   */
 
   public static boolean groupSum6(int start, int[] nums, int target) {
-
+    if (start >= nums.length){
+      return false;
+    }
+    if (nums[start] == 6){
+      return groupSum6(start + 1, nums, target - nums[start]);
+    }
+    return (groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target));
   }
 }
