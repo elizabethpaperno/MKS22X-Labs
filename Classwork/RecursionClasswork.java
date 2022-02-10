16bb3873e5b92b6097d65bf47d14890e986e1ebe
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-10 10:37:12 -0500

    RecursionClasswork groupNoAdj working

--- Classwork/RecursionClasswork.java
+++ Classwork/RecursionClasswork.java
@@ -55,4 +55,11 @@ public class RecursionClasswork{
     }
     return (groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target));
   }
+
+  public static boolean groupNoAdj(int start, int[] nums, int target) {
+    if (start >= nums.length){
+      return (target == 0);
+    }
+    return (groupNoAdj(start + 1, nums, target - nums[start]) || groupNoAdj(start + 1, nums, target));
+  }
 }
