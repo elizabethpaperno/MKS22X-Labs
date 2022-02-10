a759cfe7ce73e0a4f0e4338b4b08ee3b983a9019
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-10 10:46:57 -0500

    RecursionClasswork groupSum5 working and tested

--- Classwork/RecursionClasswork.java
+++ Classwork/RecursionClasswork.java
@@ -65,4 +65,15 @@ public class RecursionClasswork{
     }
     return (groupNoAdj(start + 2, nums, target - nums[start]) || groupNoAdj(start + 1, nums, target));
   }
+
+  public static boolean groupSum5(int start, int[] nums, int target) {
+    if (start >= nums.length){
+      return (target == 0);
+    }else if (nums[start] % 5 == 0 && (start == nums.length - 1 || nums[start + 1] != 1)){
+      return groupSum5(start + 1, nums, target - nums[start]);
+    }else if (nums[start] % 5 == 0 && nums[start + 1] == 1){
+      return (groupSum5(start + 1, nums, target - nums[start]) && groupSum5(start + 2, nums, target - nums[start]));
+    }
+    return (groupSum5(start + 1, nums, target - nums[start]) || groupSum5(start + 1, nums, target));
+  }
 }
