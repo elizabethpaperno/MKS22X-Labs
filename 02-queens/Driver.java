91d1234eaee7b50b4ac7d19fb369d3a731c12594
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-15 10:46:09 -0500

    modify to debug solve

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -1,10 +1,15 @@
 public class Driver {
   public static void main(String[] args){
-    QueenBoard board1 = new QueenBoard(8);
-    board1.addQueen(0,1) ;
-    board1.addQueen(2,5) ;
-    System.out.println(board1);
-    board1.removeQueen(0,1) ;
-    System.out.print(board1);
+    QueenBoard board1 = new QueenBoard(4);
+    //board1.addQueen(0,1) ;
+    //board1.addQueen(2,5) ;
+    //System.out.println(board1);
+    //board1.removeQueen(0,1) ;
+    //System.out.print(board1);
+    System.out.println(Text.CLEAR_SCREEN);
+    System.out.println(Text.HIDE_CURSOR);
+    System.out.println(Text.go(1,1));
+    board1.solve();
+    System.out.println(Text.RESET);
   }
 }
