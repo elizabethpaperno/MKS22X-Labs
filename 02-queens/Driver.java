0e9f16a255db0a17fc04110b22768b9817cc4490
Elizabeth Paperno <liza.paperno@gmail.com>
2022-02-14 22:18:13 -0500

    additional testing

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -1,7 +1,10 @@
 public class Driver {
   public static void main(String[] args){
     QueenBoard board1 = new QueenBoard(8);
-    board1.addQueen(1,1) ;
+    board1.addQueen(0,1) ;
+    board1.addQueen(2,5) ;
+    System.out.println(board1);
+    board1.removeQueen(0,1) ;
     System.out.print(board1);
   }
 }
