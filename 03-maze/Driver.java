10afae1323868635f78d3ef3b566cd2b738179e9
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-02 10:36:28 -0500

    class time

--- 03-maze/Driver.java
+++ 03-maze/Driver.java
@@ -3,8 +3,8 @@ import java.io.*;
 public class Driver{
   public static void main(String[] args){
     try {
-      Maze Maze1 = new Maze("Maze1.txt");
-      System.out.println(Maze1);
+      Maze Maze1 = new Maze("maze1");
+      //System.out.println(Maze1);
     }catch(FileNotFoundException e){
       System.out.println("bad file name");
     }
