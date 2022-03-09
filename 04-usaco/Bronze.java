453e6c87ef0d66d383ff7fd6b166a7e760fd0755
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-09 10:49:40 -0500

    read in grid + add toString

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class Bronze {
-  private int R,C,E,N;
+  private static int R,C,E,N;
   private int[][] grid;
   public static void parseFile(String filename) throws FileNotFoundException{
     File input = new File(filename);
@@ -11,6 +11,23 @@ public class Bronze {
     C = Character.getNumericValue(info.charAt(2));
     E = Character.getNumericValue(info.charAt(4));
     N = Character.getNumericValue(info.charAt(6));
+    int[][] grid = new int[R][C];
+    for(int row = 0; row < grid.length; row++){
+      for(int col = 0; col < grid[row].length; col++){
+        grid[row][col] = s.nextInt();
+      }
+    }
+    System.out.println(grid);
+  }
 
+  public String toString(){
+    String str = "";
+    for (int i = 0; i < grid.length; i++){
+      for (int j = 0; j < grid[i].length; j++){
+        str += grid[i][j];
+      }
+      str += "\n";
+    }
+    return str;
   }
 }
