881f4902835c400f9d47df2d14dd52cd405030fb
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-09 11:00:23 -0500

    progress on bronze

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class Bronze {
   private static int R,C,E,N;
-  private int[][] grid;
+  private static int[][] grid;
   public static void parseFile(String filename) throws FileNotFoundException{
     File input = new File(filename);
     Scanner s = new Scanner(input);
@@ -11,7 +11,7 @@ public class Bronze {
     C = Character.getNumericValue(info.charAt(2));
     E = Character.getNumericValue(info.charAt(4));
     N = Character.getNumericValue(info.charAt(6));
-    int[][] grid = new int[R][C];
+    grid = new int[R][C];
     for(int row = 0; row < grid.length; row++){
       for(int col = 0; col < grid[row].length; col++){
         grid[row][col] = s.nextInt();
@@ -24,10 +24,20 @@ public class Bronze {
     String str = "";
     for (int i = 0; i < grid.length; i++){
       for (int j = 0; j < grid[i].length; j++){
-        str += grid[i][j];
+        str += grid[i][j] + " ";
       }
       str += "\n";
     }
     return str;
+  }
+
+  public static void main(String[] args){
+    try {
+      Bronze b = new Bronze();
+      Bronze.parseFile("makelake");
+      System.out.println(b);
+    } catch (FileNotFoundException e){
+      System.out.println("Invalid filename");
+    }
   }
 }
