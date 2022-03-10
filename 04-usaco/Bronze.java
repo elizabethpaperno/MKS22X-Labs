b0a2b974fbfae704d668ed916d2039c69082ce38
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-10 11:01:16 -0500

    write stomp --> classtime

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -6,29 +6,50 @@ public class Bronze {
   public static void parseFile(String filename) throws FileNotFoundException{
     File input = new File(filename);
     Scanner s = new Scanner(input);
-    String info = s.nextLine();
-    R = Character.getNumericValue(info.charAt(0));
-    C = Character.getNumericValue(info.charAt(2));
-    E = Character.getNumericValue(info.charAt(4));
-    N = Character.getNumericValue(info.charAt(6));
+
+    R =s.nextInt();
+    C = s.nextInt();
+    E = s.nextInt();
+    N = s.nextInt();
     grid = new int[R][C];
     for(int row = 0; row < grid.length; row++){
       for(int col = 0; col < grid[row].length; col++){
         grid[row][col] = s.nextInt();
       }
     }
+    for(int i = 0; i < N; i++){
+      stomp(s.nextInt(), s.nextInt(), s.nextInt());
+    }
   }
   public static long solve(String filename){
-    parseFile(filename);
-    long total = 0;
-    for(int i = 0; i < N; i++){
-      //call stomp for input
+    try {
+      parseFile(filename);
+    }catch(FileNotFoundException e){
+      System.out.println("Invalid filename");
     }
+    long total = 0;
+
     return total;
   }
 
   public static void stomp(int Rs, int Cs, int Ds){
-    
+    int maxVal = 0;
+    for (int r  = Rs - 1; r < Rs + 4; r++){
+      for (int c = Ds - 1; c<  Rs + 4; c++ ){
+        if (grid[r][c] > maxVal){
+          maxVal = grid[r][c];
+        }
+      }
+    }
+    int finalVal = maxVal - Ds;
+    for (int r = Rs; r < Rs + 4; r++){
+      for (int c = Ds; c<  Rs + 4; c++ ){
+        if (grid[r][c] > finalVal){
+          System.out.println(grid[r][c]);
+          grid[r][c] = finalVal;
+        }
+      }
+    }
   }
   public String toString(){
     String str = "";
