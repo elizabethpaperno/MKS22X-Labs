3dda81f4319a211215d6f12965297881cc089f3e
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-13 22:48:19 -0400

    work on solve

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 public class Silver{
   private static int N, M, T;
-  private static int[][] grid;
+  private static int R1, C1, R2, C2;
+  private static long[][] grid;
   public static void parseFile(String filename) throws FileNotFoundException{
     File input = new File(filename);
     Scanner s = new Scanner(input);
@@ -11,25 +12,27 @@ public class Silver{
     M = s.nextInt();
     T = s.nextInt();
 
-    grid = new int[N][M];
-    for(int i = 0; i < grid.length; i++){
+    grid = new long[N][M];
+    for(int i = 0; i < N; i++){
+      //System.out.println(i);
       char[] row = s.nextLine().toCharArray();
-      int[] rowNum = new int[row.length];
-      for(int j = 0; j < row.length; j++){
+
+      long[] rowNum = new long[row.length];
+      for(int j = 0; j < M; j++){
         if(row[j] == '.'){
-          rowNum[i] = 0;
+          rowNum[j] = 0;
         }else{
-          rowNum[i] = -1;
+          rowNum[j] = -1;
         }
       }
       grid[i]=rowNum;
     }
-    int R1 =s.nextInt();
-    int C1 = s.nextInt();
-    int R2 = s.nextInt();
-    int C2 = s.nextInt();
 
-    solve(T, R1, C1, R2, C2);
+    R1 =s.nextInt();
+    C1 = s.nextInt();
+    R2 = s.nextInt();
+    C2 = s.nextInt();
+
   }
 
   public String toString(){
@@ -45,6 +48,56 @@ public class Silver{
 
   public static long solve(int T, int R1, int C1, int R2, int C2){
     grid[R1][C1] = 1;
-    return 1;
+    long[][] array2 = new long[N][M];
+    long up;
+    long down;
+    long right;
+    long left;
+    for (int t = 1; t < T; t++){
+      array2 = new long[N][M];
+      for (int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+          if(j == 0){
+            left = 0;
+          }else{
+            left = grid[i-1][j];
+          }
+          if (j == M - 1){
+            right = 0;
+          }else{
+            right = grid[i+1][j];
+          }
+          if(i == 0){
+            up = 0;
+          }else{
+            up = grid[i][j+1];
+          }
+          if (i == N - 1){
+            down = 0;
+          }else{
+            down = grid[i][j-1];
+          }
+          array2[i][j] = right + left + up + down;
+        }
+      }
+      grid = array2.clone();
+    }
+    return array2[R2][C2];
+  }
+  public static long solve(String filename){
+    try{
+      parseFile(filename);
+      //return solve(T, R1, C1, R2, C2);
+      return 1;
+    }catch(FileNotFoundException e){
+      System.out.println("Invalid filename");
+      return -1;
+    }
+  }
+
+  public static void main(String[] args){
+    Silver S = new Silver();
+    System.out.println(Silver.solve("ctravel.1.in"));
+    System.out.println(S);
   }
 }
