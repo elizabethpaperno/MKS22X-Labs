36047d987f0d4a5a2108b21bcce4475850ce3f59
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-14 01:38:00 -0400

    solve working

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -11,6 +11,7 @@ public class Silver{
     N =s.nextInt();
     M = s.nextInt();
     T = s.nextInt();
+    //System.out.println(T);
 
     grid = new long[N][M];
     s.nextLine();
@@ -50,43 +51,48 @@ public class Silver{
 
   public static long solve(int T, int R1, int C1, int R2, int C2){
     grid[R1][C1] = 1;
+    //System.out.println(prnt(grid));
     long[][] array2 = new long[N][M];
-    long up = 0;
-    long down = 0;
-    long right = 0;
-    long left = 0;
-    for (int t = 1; t < T; t++){
+    for (int t = 1; t <= T; t++){
+      //System.out.println("T: " + t);
+
       array2 = new long[N][M];
       for (int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
-          if (grid[i][j] == -1){
+          long up = 0;
+          long down = 0;
+          long right = 0;
+          long left = 0;
+          if(grid[i][j] == -1){
             array2[i][j] = -1;
-          }else{
-            if(i != 0){
+          }else if (grid[i][j] == 0){
+            if(i > 0){
               if (grid[i-1][j] != -1){
-                left = grid[i-1][j];
+                up = grid[i-1][j];
               }
             }
-            if (i != N - 1){
+            if (i < N - 1){
               if (grid[i+1][j] != -1){
-                right = grid[i+1][j];
+                down = grid[i+1][j];
               }
             }
-            if(j != M - 1){
+            if(j < M - 1){
               if (grid[i][j+1] != -1){
-                up = grid[i][j+1];
+                right = grid[i][j+1];
               }
             }
-            if (j != 0){
+            if (j > 0){
               if (grid[i][j-1] != -1){
-                down = grid[i][j-1];
+                left = grid[i][j-1];
               }
             }
+            array2[i][j] = right + left + up + down;
           }
-          array2[i][j] = right + left + up + down;
         }
       }
+      //System.out.println(prnt(grid));
       grid = array2.clone();
+      //System.out.println(prnt(array2));
     }
     return array2[R2][C2];
   }
@@ -102,7 +108,7 @@ public class Silver{
 
   public static void main(String[] args){
     Silver S = new Silver();
-    System.out.println(Silver.solve("ctravel.1.in"));
+    System.out.println(Silver.solve("ctravel.2.in"));
     System.out.println(S);
   }
 }
