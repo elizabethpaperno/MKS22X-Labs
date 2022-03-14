2ed619d9423ea406931ffc22ed82875788dd1f95
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-13 23:07:06 -0400

    changes made to solve

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -13,12 +13,14 @@ public class Silver{
     T = s.nextInt();
 
     grid = new long[N][M];
+    s.nextLine();
     for(int i = 0; i < N; i++){
       //System.out.println(i);
       char[] row = s.nextLine().toCharArray();
 
       long[] rowNum = new long[row.length];
-      for(int j = 0; j < M; j++){
+      //System.out.println(Arrays.toString(row));
+      for(int j = 0; j < row.length; j++){
         if(row[j] == '.'){
           rowNum[j] = 0;
         }else{
@@ -49,33 +51,37 @@ public class Silver{
   public static long solve(int T, int R1, int C1, int R2, int C2){
     grid[R1][C1] = 1;
     long[][] array2 = new long[N][M];
-    long up;
-    long down;
-    long right;
-    long left;
+    long up = 0;
+    long down = 0;
+    long right = 0;
+    long left = 0;
     for (int t = 1; t < T; t++){
       array2 = new long[N][M];
       for (int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
-          if(j == 0){
-            left = 0;
+          if (grid[i][j] == -1){
+            array2[i][j] = -1;
           }else{
-            left = grid[i-1][j];
-          }
-          if (j == M - 1){
-            right = 0;
-          }else{
-            right = grid[i+1][j];
-          }
-          if(i == 0){
-            up = 0;
-          }else{
-            up = grid[i][j+1];
-          }
-          if (i == N - 1){
-            down = 0;
-          }else{
-            down = grid[i][j-1];
+            if(j != 0){
+              if (grid[i-1][j] != -1){
+                left = grid[i-1][j];
+              }
+            }
+            if (j != M - 1){
+              if (grid[i+1][j] != -1){
+                right = grid[i+1][j];
+              }
+            }
+            if(i != 0){
+              if (grid[i][j+1] != -1){
+                up = grid[i][j+1];
+              }
+            }
+            if (i != N - 1){
+              if (grid[i][j-1] != -1){
+                down = grid[i][j-1];
+              }
+            }
           }
           array2[i][j] = right + left + up + down;
         }
@@ -87,8 +93,7 @@ public class Silver{
   public static long solve(String filename){
     try{
       parseFile(filename);
-      //return solve(T, R1, C1, R2, C2);
-      return 1;
+      return solve(T, R1, C1, R2, C2);
     }catch(FileNotFoundException e){
       System.out.println("Invalid filename");
       return -1;
