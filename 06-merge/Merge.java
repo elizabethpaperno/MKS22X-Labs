b137abb09dbf81dde0d6d3c9cc94486a1079b210
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-21 10:58:37 -0400

    mergeSortH filled in --> not tested + class time

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,9 +1,9 @@
 public class Merge {
-  int [] merge(int [] left, int[] right){
+  public static int [] merge(int [] left, int[] right){
     //return a new array that is the merged version of left and right
   }
 
-  void mergesort(int[] data){
+  public static void mergesort(int[] data){
     int [] temp  =  mergeSortH(data);
     //copy temp into data
     for(int i = 0; i < temp.length(); i++){
@@ -11,11 +11,21 @@ public class Merge {
     }
   }
 
-  int[] mergesortH(data){
-    if more than 1 element{
+  public static int[] mergesortH(int[] data){
+    if (data.length > 0){
+      int[] left = new int[data.length/2];
+      for (int i = 0; i < left.length; i++){
+        left[i] = data[i];
+      }
+      int[] right = new int[data.length - left.length];
+      int dataIn = (data.length - left.length) - 1;
+      for (int i = 0; i < left.length; i++){
+        left[i] = data[dataIn];
+        dataIn += 1;
+      }
       //left = copy half of data
       //right = copy other half of data
-      return merge(mergesortH(left),mergesortH(right))
+      return merge(mergesortH(left),mergesortH(right));
     }
   }
 }
