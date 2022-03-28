880c99f097ea4dd5ef0608e7bd7ddb7f1e1e667e
Elizabeth Paperno <liza.paperno@gmail.com>
2022-03-28 10:53:41 -0400

    no wraparound + no exceptions --> some method filled in

---
+++ 07-mydeque/MyDeque.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class MyDeque<E>{
+  private E[] data;
+  private int size;
+  private int start, end;
+
+  public MyDeque(){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
+    size = 0;
+    start = 10/2 - 1;
+    end = start + 1;
+  }
+  public MyDeque(int initialCapacity){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+    size = 0;
+    start = initialCapacity/2 - 1;
+    end = start + 1;
+  }
+  public int size(){
+    return size;
+  }
+  public String toString(){
+    //fill in later
+    /*
+    String str = "[";
+    for(int i = start; i < this.size(); i++){
+      str += data[i] + ", ";
+    }
+
+    return str;
+    */
+  }
+  public String toStringDebug(){
+    return Arrays.toString(data);
+  }
+  public void addFirst(E element){
+    resize();
+    //ADD CONDITION TO WRAP AROUND
+    start -= 1;
+    data[start] = element;
+  }
+  public void addLast(E element){
+    //ADD CONDITION TO WRAP AROUND
+    resize();
+    end += 1;
+    data[end] = element;
+  }
+  public E removeFirst(){
+    E first = data[start];
+    data[start] = null;
+    //ADD CONDITION TO WRAP AROUND
+    start += 1;
+    size -= 1;
+    return first;
+  }
+  public E removeLast(){
+    E last = data[end];
+    data[end] = null;
+    //ADD CONDITION TO WRAP AROUND
+    end -= 1;
+    size -= 1;
+    return last;
+  }
+  public E getFirst(){
+    return data[start];
+  }
+  public E getLast(){
+    return data[end];
+  }
+  private void resize(){
+    if (this.size() == data.length){
+      @SuppressWarnings("unchecked")
+      E[] d = (E[])new Object[data.length * 2 + 1];
+      for (int i = 0; i < this.size(); i++){
+        d[i] = data[i];
+      }
+      this.data = d;
+    }
+  }
+}
