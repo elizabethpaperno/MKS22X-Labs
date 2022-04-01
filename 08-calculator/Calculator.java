b594bd46267e3b1776730c229db870de67ca3a20
Elizabeth Paperno <liza.paperno@gmail.com>
2022-04-01 10:52:04 -0400

    working with all exceptions thrown

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -14,41 +14,80 @@ public class Calculator{
     ArrayList<String> list = new ArrayList<String>();
     Scanner str = new Scanner(s);
 
+    //create list of vals
     while(str.hasNext()){
       list.add(str.next());
     }
 
-    System.out.println(list);
+    //throw exception for case of empty string
+    if (list.size() == 0){
+      throw new IllegalArgumentException("too few operands");
+    }
+
+    //if one value just return it
+    if (list.size() == 1){
+      return Double.parseDouble(list.get(0));
+    }
+
+
     for(int i = 0; i < list.size(); i++){
       if (list.get(i).equals("*")){
         //multiplication
+        if (stack.size() < 2){
+          throw new IllegalArgumentException("too few operands for operation *");
+        }
         stack.addLast(stack.pop() * stack.pop());
       }else if (list.get(i).equals("+")){
         //addition
+        if (stack.size() < 2){
+          throw new IllegalArgumentException("too few operands for operation +");
+        }
         stack.addLast(stack.pop() + stack.pop());
       }else if (list.get(i).equals("/")){
         //subtraction
+        if (stack.size() < 2){
+          throw new IllegalArgumentException("too few operands for operation /");
+        }
         double pop1 = stack.pop();
         double pop2 = stack.pop();
         stack.addLast(pop1 / pop2);
       }else if (list.get(i).equals("-")){
         //subtraction
+        if (stack.size() < 2){
+          throw new IllegalArgumentException("too few operands for operation -");
+        }
         double pop1 = stack.pop();
         double pop2 = stack.pop();
         stack.addLast(pop1 - pop2);
       }else if (list.get(i).equals("%")){
+        if (stack.size() < 2){
+          throw new IllegalArgumentException("too few operands for operation %");
+        }
         double pop1 = stack.pop();
         double pop2 = stack.pop();
         stack.addLast(pop1 % pop2);
       }else{
         stack.addLast(Double.parseDouble(list.get(i)));
       }
-      System.out.println(stack);
+
+    }
+    if (stack.size() > 1){
+      throw new IllegalArgumentException("too many operands");
     }
     return stack.getFirst();
   }
+
+
   public static void main(String[] args){
     System.out.println(eval("11 3 - 4 + 2.5 *"));
     System.out.println(eval("10 2.0 +"));
+    System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+    System.out.println(eval("1 2 3 4 5 + * - -"));
+    //System.out.println(eval("4 2 3 5 1 - + * + *"));
+    //System.out.println(eval("5 3 7 9 + +"));
+    //System.out.println(eval("25"));
+    System.out.println(eval(""));
+    //System.out.println(eval("1 1 1 +"));
+    //System.out.println(eval("1 1 1 - * +"));
   }
 }
